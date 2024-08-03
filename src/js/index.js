94c026739e74b468d5dc059c218f97b2525a8016
Jahn1295 <jiheeahn1222@gmail.com>
2024-08-03 00:49:45 +0000

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-//import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import "../styles/index.css";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -9,5 +9,60 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0;
+
+const SimpleCounter = ({ count }) => {
+    const fourth = Math.floor(count / 1000) % 10;
+    const third = Math.floor(count / 100) % 10;
+    const second = Math.floor(count / 10) % 10;
+    const first = count % 10;
+
+    return (
+        <div className="clockBody">
+            <div className="icon">
+            <i class="fa-regular fa-clock"></i>
+            </div>
+            <div className="fourthDigit">{fourth}</div>
+            <div className="thirdDigit">{third}</div>
+            <div className="secondDigit">{second}</div>
+            <div className="firstDigit">{first}</div>
+        </div>
+    );
+};
+
+const Button = ({ onStopClick }) => {
+    return (
+        <button onClick={onStopClick}>
+            Stop
+        </button>
+    );
+};
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [isRunning, setIsRunning] = useState(true);
+
+    useEffect(() => {
+        let intervalId;
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setCount((prevCount) => prevCount + 1);
+            }, 1000);
+        }
+        return () => clearInterval(intervalId);
+    }, [isRunning]);
+
+    const handleStop = () => {
+        setIsRunning(false);
+    };
+
+    return (
+        <>
+            <SimpleCounter count={count} />
+            <Button onStopClick={handleStop} />
+        </>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
 
